4e7decd5a13bdc64f145f360c2574eeb3ce6a68c
Andrew Fairbairn <AndrewFairbairn@users.noreply.github.com>
2019-11-01 13:41:59 +0100

    - Recurse through list sub-types and clean NullOrEmpty values from them too

--- src/dictionary/sanitise.py
+++ src/dictionary/sanitise.py
@@ -10,33 +10,67 @@ def sanitise(item):
     elif isinstance(item_to_sanitise, (list, str)):
         if len(item_to_sanitise) == 0:
             return None
+        elif isinstance(item_to_sanitise, list):
+            return _sanitise_list(item_to_sanitise)
 
     return item_to_sanitise
 
 
-def _sanitise_dictionary(dictionary):
+def _sanitise_dictionary(item):
     # We must copy this object as we will iterate through it but may remove nodes from it
-    dict_to_sanitise = dictionary.copy()
-    for key, value in dictionary.items():
+    item_to_sanitise = item.copy()
+    for key, value in item.items():
         # If the key is a dictionary then pass the value back into this method again
         if isinstance(value, dict):
-            if len(dict_to_sanitise[key]) == 0:
-                del dict_to_sanitise[key]
+            if len(item_to_sanitise[key]) == 0:
+                del item_to_sanitise[key]
             else:
                 sanitised_key = sanitise(value)
 
                 if sanitised_key is None:
-                    del dict_to_sanitise[key]
+                    del item_to_sanitise[key]
                 else:
-                    dict_to_sanitise[key] = sanitised_key
+                    item_to_sanitise[key] = sanitised_key
 
         elif value is None or value == "":
-            del dict_to_sanitise[key]
+            del item_to_sanitise[key]
         else:
             sanitised_key = sanitise(value)
 
             if sanitised_key is None:
-                del dict_to_sanitise[key]
+                del item_to_sanitise[key]
             else:
-                dict_to_sanitise[key] = sanitised_key
-    return dict_to_sanitise
+                item_to_sanitise[key] = sanitised_key
+    return item_to_sanitise
+
+
+def _sanitise_list(item):
+    item_to_sanitise = item.copy()
+    sanitised_index = 0
+    for list_item in item:
+        sanitised_index = sanitised_index + 1
+        if list_item is None:
+            item_to_sanitise.remove(list_item)
+            sanitised_index = sanitised_index - 1
+        elif isinstance(list_item, (str, list, dict)):
+            if len(list_item) == 0:
+                item_to_sanitise.remove(list_item)
+                sanitised_index = sanitised_index - 1
+            elif isinstance(list_item, dict):
+                sanitised_item = _sanitise_dictionary(list_item)
+                if sanitised_item is None:
+                    item_to_sanitise.remove(list_item)
+                    sanitised_index = sanitised_index - 1
+                else:
+                    item_to_sanitise.remove(list_item)
+                    item_to_sanitise.insert(sanitised_index, sanitised_item)
+            elif isinstance(list_item, list):
+                sanitised_item = _sanitise_list(list_item)
+                if sanitised_item is None:
+                    item_to_sanitise.remove(list_item)
+                    sanitised_index = sanitised_index - 1
+                else:
+                    item_to_sanitise.remove(list_item)
+                    item_to_sanitise.insert(sanitised_index, sanitised_item)
+
+    return item_to_sanitise
